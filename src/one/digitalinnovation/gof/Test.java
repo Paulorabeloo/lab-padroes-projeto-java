7d058100af6d4d1c1ac459ef3fa4611b4bc9d270
Paulorabeloo <pauloeduardorabelomiranda@gmail.com>
2024-07-29 21:47:57 -0300

    Melhorias

--- src/one/digitalinnovation/gof/Test.java
+++ src/one/digitalinnovation/gof/Test.java
@@ -12,46 +12,60 @@ import one.digitalinnovation.gof.strategy.Robo;
 
 public class Test {
 
-	public static void main(String[] args) {
-		
-		// Singleton
-		
-		SingletonLazy lazy = SingletonLazy.getInstancia();
-		System.out.println(lazy);
-		lazy = SingletonLazy.getInstancia();
-		System.out.println(lazy);
-		
-		SingletonEager eager = SingletonEager.getInstancia();
-		System.out.println(eager);
-		eager = SingletonEager.getInstancia();
-		System.out.println(eager);
-		
-		SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
-		System.out.println(lazyHolder);
-		lazyHolder = SingletonLazyHolder.getInstancia();
-		System.out.println(lazyHolder);
-		
-		// Strategy
-		
-		Comportamento defensivo = new ComportamentoDefensivo();
-		Comportamento normal = new ComportamentoNormal();
-		Comportamento agressivo = new ComportamentoAgressivo();
-		
-		Robo robo = new Robo();
-		robo.setComportamento(normal);
-		robo.mover();
-		robo.mover();
-		robo.setComportamento(defensivo);
-		robo.mover();
-		robo.setComportamento(agressivo);
-		robo.mover();
-		robo.mover();
-		robo.mover();
-		
-		// Facade
-		
-		Facade facade = new Facade();
-		facade.migrarCliente("Venilton", "14801788");
-	}
-
+    public static void main(String[] args) {
+        
+        // Demonstrando o padrão Singleton
+        
+        System.out.println("Demonstrando Singleton:");
+        
+        // Singleton Lazy
+        SingletonLazy lazy1 = SingletonLazy.getInstancia();
+        System.out.println("SingletonLazy 1: " + lazy1);
+        SingletonLazy lazy2 = SingletonLazy.getInstancia();
+        System.out.println("SingletonLazy 2: " + lazy2);
+        
+        // Singleton Eager
+        SingletonEager eager1 = SingletonEager.getInstancia();
+        System.out.println("SingletonEager 1: " + eager1);
+        SingletonEager eager2 = SingletonEager.getInstancia();
+        System.out.println("SingletonEager 2: " + eager2);
+        
+        // Singleton Lazy Holder
+        SingletonLazyHolder lazyHolder1 = SingletonLazyHolder.getInstancia();
+        System.out.println("SingletonLazyHolder 1: " + lazyHolder1);
+        SingletonLazyHolder lazyHolder2 = SingletonLazyHolder.getInstancia();
+        System.out.println("SingletonLazyHolder 2: " + lazyHolder2);
+        
+        // Demonstrando o padrão Strategy
+        
+        System.out.println("\nDemonstrando Strategy:");
+        
+        Comportamento comportamentoDefensivo = new ComportamentoDefensivo();
+        Comportamento comportamentoNormal = new ComportamentoNormal();
+        Comportamento comportamentoAgressivo = new ComportamentoAgressivo();
+        
+        Robo robo = new Robo();
+        
+        // Robo com comportamento normal
+        robo.setComportamento(comportamentoNormal);
+        robo.mover();
+        robo.mover();
+        
+        // Robo com comportamento defensivo
+        robo.setComportamento(comportamentoDefensivo);
+        robo.mover();
+        
+        // Robo com comportamento agressivo
+        robo.setComportamento(comportamentoAgressivo);
+        robo.mover();
+        robo.mover();
+        robo.mover();
+        
+        // Demonstrando o padrão Facade
+        
+        System.out.println("\nDemonstrando Facade:");
+        
+        Facade facade = new Facade();
+        facade.migrarCliente("Venilton", "14801788");
+    }
 }
